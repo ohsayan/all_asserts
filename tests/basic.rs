48bfec5771aff27863e2d92d1fc14cb73360db84
Sayan <nandansayan@outlook.com>
2026-06-21 11:03:45 +0530

    tests: Improve test coverage (#12)

--- tests/basic.rs
+++ tests/basic.rs
@@ -3,52 +3,176 @@ use all_asserts::{
     assert_range, assert_true,
 };
 
+/*
+    numbers
+*/
+
+// ge
+#[test]
+fn assert_ge_ok() {
+    assert_ge!(5, 4);
+    assert_ge!(4, 4);
+}
+#[test]
+fn assert_ge_ok_msg() {
+    assert_ge!(5, 4, "shouldn't print");
+    assert_ge!(4, 4, "shouldn't print");
+}
 #[test]
 #[should_panic]
-fn panic_when_not_gt() {
+fn assert_ge_fail() {
+    assert_ge!(3, 4);
+}
+#[test]
+#[should_panic = "that failed"]
+fn assert_ge_fail_msg() {
+    assert_ge!(3, 4, "that failed");
+}
+
+// gt
+#[test]
+fn assert_gt_ok() {
+    assert_gt!(5, 4);
+}
+#[test]
+fn assert_gt_ok_msg() {
+    assert_gt!(5, 4, "shouldn't print");
+}
+#[test]
+#[should_panic]
+fn assert_gt_fail_1() {
     assert_gt!(4, 4);
 }
 #[test]
 #[should_panic]
-fn panic_when_not_ge() {
-    assert_ge!(3, 4);
+fn assert_gt_fail_2() {
+    assert_gt!(3, 4);
+}
+#[test]
+#[should_panic = "that failed"]
+fn assert_gt_fail_msg() {
+    assert_gt!(3, 4, "that failed");
+}
+
+// le
+#[test]
+fn assert_le_ok() {
+    assert_le!(3, 4);
+    assert_le!(3, 3);
+}
+#[test]
+fn assert_le_ok_msg() {
+    assert_le!(3, 4, "shouldn't print");
+    assert_le!(3, 3, "shouldn't print");
 }
 #[test]
 #[should_panic]
-fn panic_when_not_lt() {
+fn assert_le_fail() {
+    assert_le!(5, 3);
+}
+#[test]
+#[should_panic = "that failed"]
+fn assert_le_fail_msg() {
+    assert_le!(5, 3, "that failed");
+}
+
+// lt
+#[test]
+fn assert_lt_ok() {
+    assert_lt!(4, 5);
+}
+#[test]
+fn assert_lt_ok_msg() {
+    assert_lt!(4, 5, "shouldn't print");
+}
+#[test]
+#[should_panic]
+fn assert_lt_fail_1() {
     assert_lt!(4, 4);
 }
 #[test]
 #[should_panic]
-fn panic_when_not_le() {
-    assert_le!(4, 3);
+fn assert_lt_fail_2() {
+    assert_lt!(5, 4);
 }
 #[test]
-fn test_assert_range_pass() {
+#[should_panic = "5 < 4 is false"]
+fn assert_lt_fail_msg() {
+    assert_lt!(5, 4, "5 < 4 is false");
+}
+
+/*
+    boolean
+*/
+
+#[test]
+fn assert_false_ok() {
+    assert_false!(false);
+    assert_false!(10 > 20);
+}
+#[test]
+#[should_panic]
+fn assert_false_fail() {
+    assert_false!(true);
+}
+#[test]
+#[should_panic(expected = "a = 3, b = 27")]
+fn assert_false_fail_msg() {
+    let a = 3;
+    let b = 27;
+    assert_false!(a + b == 30, "a = {}, b = {}", a, b);
+}
+#[test]
+fn assert_false_ok_msg() {
+    let a = 3;
+    let b = 28;
+    assert_false!(a + b == 30, "a = {}, b = {}", a, b);
+}
+
+#[test]
+fn assert_true_ok() {
+    assert_true!(true);
+    assert_true!(20 > 10);
+}
+#[test]
+#[should_panic]
+fn assert_true_fail() {
+    assert_true!(false);
+}
+#[test]
+#[should_panic(expected = "a = 3, b = 28")]
+fn assert_true_fail_msg() {
+    let a = 3;
+    let b = 28;
+    assert_true!(a + b == 30, "a = {}, b = {}", a, b);
+}
+#[test]
+fn assert_true_ok_msg() {
+    let a = 3;
+    let b = 27;
+    assert_true!(a + b == 30, "a = {}, b = {}", a, b);
+}
+
+/*
+    range
+*/
+
+// range
+#[test]
+fn assert_range_pass() {
     assert_range!(0..=10, 10);
     assert_range!(1.0..2.0, 1.5);
     assert_range!(1.0..2.0, 1.3);
 }
 #[test]
 #[should_panic]
-fn test_assert_range_fail() {
+fn assert_range_fail() {
     assert_range!(0..10, 11);
     assert_range!(1.0..=2.0, 2.1);
 }
 #[test]
-fn test_assert_nrange_pass() {
-    assert_nrange!(10..20, 20);
-    assert_nrange!(1.0..2.0, 2.0);
-}
-#[test]
 #[should_panic]
-fn test_assert_nrange_fail() {
-    assert_nrange!(10..=20, 20);
-    assert_nrange!(1.0..=2.0, 2.0);
-}
-#[test]
-#[should_panic]
-fn test_assert_range_with_fail_msg() {
+fn assert_range_fail_msg() {
     // Assert with a message
     assert_range!(
         1.0..=2.0,
@@ -56,14 +180,32 @@ fn test_assert_range_with_fail_msg() {
         "Failed to assert that 2.1 is in the interval [1.0,2.0]"
     );
 }
+
+// nrange
+#[test]
+fn assert_nrange_pass() {
+    assert_nrange!(10..20, 20);
+    assert_nrange!(1.0..2.0, 2.0);
+}
 #[test]
 #[should_panic]
-fn test_assert_nrange_with_fail_msg() {
+fn assert_nrange_fail() {
+    assert_nrange!(10..=20, 20);
+    assert_nrange!(1.0..=2.0, 2.0);
+}
+#[test]
+#[should_panic]
+fn assert_nrange_fail_msg() {
     // Assert with a message
     assert_nrange!(1.0..2.0, 1.5, "Oops! 1.5 is in the interval [1.0,2.0)")
 }
+
+/*
+    boundary
+*/
+
 #[test]
-fn test_assert_near_pass() {
+fn assert_near_pass() {
     assert_near!(0.0, 0.0, 0.1);
     assert_near!(1.0f32, (1.0f32 + f32::EPSILON), f32::EPSILON);
     assert_near!(1.0f32, (1.0f32 - f32::EPSILON), f32::EPSILON);
@@ -72,57 +214,13 @@ fn test_assert_near_pass() {
 #[should_panic(
     expected = "assertion failed: `1.0000002 is not within epsilon 0.00000011920929 of 1"
 )]
-fn test_assert_near_fail_high() {
+fn assert_near_fail_high() {
     assert_near!(1.0f32, (1.0f32 + f32::EPSILON + f32::EPSILON), f32::EPSILON);
 }
 #[test]
 #[should_panic(
     expected = "assertion failed: `0.99999976 is not within epsilon 0.00000011920929 of 1"
 )]
-fn test_assert_near_fail_low() {
+fn assert_near_fail_low() {
     assert_near!(1.0f32, (1.0f32 - f32::EPSILON - f32::EPSILON), f32::EPSILON);
-}
-#[test]
-#[should_panic]
-fn panic_when_not_true() {
-    assert_true!(false);
-}
-#[test]
-fn do_not_panic_when_true() {
-    assert_true!(true);
-}
-#[test]
-#[should_panic(expected = "a = 3, b = 28")]
-fn panic_when_not_true_including_message() {
-    let a = 3;
-    let b = 28;
-    assert_true!(a + b == 30, "a = {}, b = {}", a, b);
-}
-#[test]
-fn do_not_panic_when_true_and_do_not_emit_message() {
-    let a = 3;
-    let b = 27;
-    assert_true!(a + b == 30, "a = {}, b = {}", a, b);
-}
-#[test]
-#[should_panic]
-fn panic_when_not_false() {
-    assert_false!(true);
-}
-#[test]
-fn do_not_panic_when_false() {
-    assert_false!(false);
-}
-#[test]
-#[should_panic(expected = "a = 3, b = 27")]
-fn panic_when_not_false_including_message() {
-    let a = 3;
-    let b = 27;
-    assert_false!(a + b == 30, "a = {}, b = {}", a, b);
-}
-#[test]
-fn do_not_panic_when_false_and_do_not_emit_message() {
-    let a = 3;
-    let b = 28;
-    assert_false!(a + b == 30, "a = {}, b = {}", a, b);
 }
